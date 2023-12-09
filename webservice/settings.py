d919061cafa974723339d9daa94cc3691526189b
jejevj <wijaya.angga.j@gmail.com>
2023-12-09 19:08:44 +0700

    origin allowed

--- webservice/settings.py
+++ webservice/settings.py
@@ -31,7 +31,11 @@ ALLOWED_HOSTS = [
     '127.0.0.1'
 ]
 
-
+CORS_ALLOWED_ORIGINS =[
+    'https://garudadriver.azurewebsites.net/',
+    'http://garudadriver.azurewebsites.net/'
+    
+]
 # Application definition
 
 INSTALLED_APPS = [
@@ -131,12 +135,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  
-STATIC_ROOT = os.path.join(BASE_DIR, '')
+
+STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
+STATIC_URL = 'static/'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# print(STATIC_ROOT)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
