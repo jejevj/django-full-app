5b90e8a038d07aaba67a1207c087e88a84bb37b4
jejevj <wijaya.angga.j@gmail.com>
2023-12-09 18:53:51 +0700

    static problem

--- webservice/settings.py
+++ webservice/settings.py
@@ -136,7 +136,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
